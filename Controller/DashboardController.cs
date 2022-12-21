0a375c8a8ddc758ee3704ca654abc876c8c58ffa
Nombuso <38411152+Insitepass@users.noreply.github.com>
2022-12-22 00:11:09 +0200

    Add files via upload

---
+++ Controller/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using DevExpress.DashboardAspNetCore;
+using DevExpress.DashboardWeb;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PortalPreview.Controller
+{
+    public class DefaultDashboardController : DashboardController
+    {
+        public DefaultDashboardController(DashboardConfigurator configurator, IDataProtectionProvider? dataProtectionProvider = null)
+           : base(configurator, dataProtectionProvider)
+        {
+        }
+    }
+}
